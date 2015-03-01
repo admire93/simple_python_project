406481aa735ffbdb753b6a5345e5dabb78556862
Kang Hyojun <hyojun@admire.kr>
2015-03-01 23:30:39 +0900

    Reperesent memory address either

--- simple/mixin.py
+++ simple/mixin.py
@@ -12,9 +12,8 @@ class BaseMixin(object):
     __repr_attr__ = 'id',
 
     def __repr__(self):
-        print()
         represents = []
         for attr in self.__repr_attr__:
             represents.append('{}={}'.format(attr, getattr(self, attr)))
-        return '{0.__class__.__name__}({1})'.format(self,
-                                                    ', '.join(represents))
+        return '{0.__class__.__name__}({1}) @ {2}'.format(
+            self, ', '.join(represents), hex(id(self)))
--- tests/mixin_test.py
+++ tests/mixin_test.py
@@ -29,6 +29,6 @@ def test_base_mixin(fx_session):
     ss = ss[0]
     assert ss.id
     assert ss.created_at
-    expected = 'SuperSimpleData(id={}, created_at={})'.format(
-        ss.id, ss.created_at)
+    expected = 'SuperSimpleData(id={}, created_at={}) @ {}'.format(
+        ss.id, ss.created_at, hex(id(ss)))
     assert expected == repr(ss)
