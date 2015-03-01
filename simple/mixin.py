14eb0dbf46565a272ba63f47bdff97b46ca4ae0b
Kang Hyojun <hyojun@admire.kr>
2015-03-01 22:40:46 +0900

    Add mixin test

--- simple/mixin.py
+++ simple/mixin.py
@@ -9,7 +9,12 @@ class BaseMixin(object):
 
     created_at = Column(DateTime, default=datetime.now, nullable=False)
 
-    __repr_attr__ = ()
+    __repr_attr__ = 'id',
 
     def __repr__(self):
-        return '{}'.format(self.__repr_attr__)
+        print()
+        represents = []
+        for attr in self.__repr_attr__:
+            represents.append('{}={}'.format(attr, getattr(self, attr)))
+        return '{0.__class__.__name__}({1})'.format(self,
+                                                    ', '.join(represents))
---
+++ tests/mixin_test.py
@@ -0,0 +1,34 @@
+from pytest import yield_fixture
+from sqlalchemy.types import Unicode
+
+from simple.app import app, db
+from simple.mixin import BaseMixin
+
+
+class SuperSimpleData(db.Model, BaseMixin):
+
+    __tablename__ = 'simple'
+
+    __repr_attr__ = 'id', 'created_at'
+
+
+@yield_fixture
+def fx_session(fx_app):
+    with fx_app.app_context():
+        db.create_all()
+        yield db.session
+        db.drop_all()
+
+
+def test_base_mixin(fx_session):
+    ss = SuperSimpleData()
+    fx_session.add(ss)
+    fx_session.commit()
+    ss = SuperSimpleData.query.all()
+    assert ss
+    ss = ss[0]
+    assert ss.id
+    assert ss.created_at
+    expected = 'SuperSimpleData(id={}, created_at={})'.format(
+        ss.id, ss.created_at)
+    assert expected == repr(ss)
