bb9225a000499b3da7aab935ce5897136939372e
Kang Hyojun <hyojun@admire.kr>
2015-03-01 22:45:16 +0900

    Add structure for web

--- simple/app.py
+++ simple/app.py
@@ -16,3 +16,37 @@ def create_app():
 
 app = create_app()
 db = SQLAlchemy(app)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return ''
+
+
+@app.route('/login/', methods=['GET'])
+def login():
+    return ''
+
+
+@app.route('/login/', methods=['POST'])
+def do_login():
+    return ''
+
+
+@app.route('/logout/', methods=['GET'])
+def logout():
+    return ''
+
+
+@app.route('/tags/', methods=['GET'])
+def tags():
+    return ''
+
+
+@app.route('/tags/', methods=['POST'])
+def create_tags():
+    return ''
+
+
+@app.route('/users/<name>/statistics/', methods=['GET'])
+def user_statistics(name):
+    return ''
